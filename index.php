7a01166d904a9147c87b73e94effd3816ab80109
UserC0de <43649283+UserC0de@users.noreply.github.com>
2024-04-30 16:46:24 +0200

    Plantilla Portal

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Casino LA CAMPIÑA</title>
+  <script src="script.js"></script>
+  <link rel="stylesheet" href="Miestilos.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+  <div id="container">
+    <header>
+      <div class="p-2 m-auto">
+        <nav class="navbar navbar-expand text-bg-warning fw-bold rounded-4">
+          <div class="container-fluid text-uppercase">
+            <a class="navbar-brand" href="index.html">
+              <img src="fotos/xdxd.png" alt="" class="px-3">
+            </a>
+            <div class="display-5 text-light">
+              <h1 class="fw-bold m-auto ms-3">LA CAMPIÑA</h1>
+            </div>
+            <ul class="navbar-nav fw-bold">
+              <li class="nav-item">
+                <a class="nav-link active text-light" aria-current="page" href="#">Apuestas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Apuestas en vivo</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Porras</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Casino</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Slots</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Virtuales</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Locales</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item m-auto">
+              <a href="login.php"><button type="button" class="btn btn-light p-2 me-2 fw-bold text-warning">Entrar</button></a>
+              </li>
+
+              <li class="nav-item m-auto p-2">
+                <button type="button" class="btn btn-light p-2 me-2 fw-bold text-warning">Registrarse</button>
+              </li>
+            </ul>
+        </nav>
+      </div>
+  </div>
+  </header>
+
+  <main>
+    <div class="row mt-3 mx-auto">
+      <div class="col-sm-4">
+        <div class="card text-white border-light">
+          <img src="fotos/card1.png" class="card-img" alt="..." style="max-width: 600px;">
+          <div class="card-img-overlay">
+            <h1 class="card-title fw-bold text-light mt-4 mx-3">Más de</h1>
+            <p class="card-text h2 fw-bold text-muted mx-3">60 mesas</p>
+            <p class="card-text h4 fw-bold text-light mx-3">Ruletas, Blackjack...</p>
+            <button type="button" class="btn btn-info mt-3 mx-5 text-dark p-3">REGISTRARSE</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card text-white border-light">
+          <img src="fotos/card2.png" class="card-img" alt="..." style="max-width: 600px;">
+          <div class="card-img-overlay">
+            <h5 class="card-title h4 text-light fw-bold mt-4 mb-lg-3 mx-3">Increíbles cuotas en</h5>
+            <p class="card-text h1 fw-bold text-muted mx-3">FÚTBOL</p>
+            <p class="card-text h5 text-light fw-bold mx-3 mt-3">Si te haces cliente</p>
+            <button type="button" class="btn btn-info mt-3 mx-5 text-dark p-3">REGISTRARSE</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card text-white border-light">
+          <img src="fotos/card3.png" class="card-img" alt="..." style="max-width: 600px;">
+          <div class="card-img-overlay">
+            <h5 class="card-title h4 fw-bold mt-4 mx-3">Toda la previa de</h5>
+            <p class="card-text h1 fw-bold mx-3 text-muted">ROLAND GARROS</p>
+            <p class="card-text h4 fw-bold mx-3">En streaming</p>
+            <button type="button" class="btn btn-info mt-3 mx-5 text-dark p-3">REGISTRARSE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <a href="#" class="text-dark">
+      <div class="row row-cols-md-4 col-sm-6 mx-auto mt-4">
+        <div class="card border-light">
+          <img src="fotos/cardapuestas.png" class="card-img-top mx-auto" alt="..." style="max-width: 240px;">
+          <div class="card-body mx-auto p-0">
+            <img src="fotos/football-outline-circle.svg" alt="" class="card-text">
+            <p class="card-text fw-bold h5">APUESTAS</p>
+          </div>
+        </div>
+    </a>
+
+    <a href="#" class="text-dark">
+      <div class="card border-light">
+        <img src="fotos/cardcasino.png" class="card-img-top mx-auto" alt="..." style="max-width: 240px;">
+        <div class="card-body m-auto p-0">
+          <img src="fotos/roulette-outline-circle.svg" alt="" class="card-text">
+          <p class="card-text fw-bold h5">CASINO</p>
+        </div>
+      </div>
+    </a>
+
+    <a href="#" class="text-dark">
+      <div class="card border-light">
+        <img src="fotos/cardslots.png" class="card-img-top mx-auto" alt="..." style="max-width: 240px;">
+        <div class="card-body mx-auto p-0">
+          <img src="fotos/slot-outline-circle.svg" alt="" style="max-width: 100px;" class="card-text">
+          <p class="card-text fw-bold h5">SLOTS</p>
+        </div>
+      </div>
+    </a>
+
+    <a href="#" class="text-dark">
+      <div class="card border-light">
+        <img src="fotos/cardblog.png" class="card-img-top mx-auto" alt="..." style="max-width: 240px;">
+        <div class="card-body mx-auto p-0">
+          <img src="fotos/Blog-icon.svg" alt="" style="max-width: 100px;" class="card-text">
+          <p class="card-text fw-bold h5">BLOG</p>
+        </div>
+      </div>
+    </a>
+    </div>
+
+  </main>
+
+  <!--Aquí se configura el footer-->
+  <footer class="text-white text-center text-lg-start bg-dark">
+
+    <div class="container p-4">
+
+      <div class="row mt-4">
+
+        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Sobre nosotros</h5>
+
+          <p>
+            La campiña está autorizado por la Dirección General de Ordenación del Juego
+          </p>
+
+          <p>
+            La campiña es una de las principales casas españolas en apuestas online y juegos de Casino.
+            Diviértete con nosotros y disfruta de una experiencia de juego segura.
+          </p>
+
+          <div class="mt-4">
+            <!-- Telegram -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><img
+                src="bootstrap-icons/icons/telegram.svg"></a>
+            <!-- Wasap -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><img
+                src="bootstrap-icons/icons/whatsapp.svg"></a>
+            <!-- Twitter -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><img src="bootstrap-icons/icons/twitter.svg"></a>
+            <!-- Facebook -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><img
+                src="bootstrap-icons/icons/facebook.svg"></a>
+          </div>
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4 pb-1">Buscador</h5>
+          <!--Barra de buscar-->
+          <div class="form-outline form-white mb-4">
+            <input type="text" id="formControlLg" class="form-control form-control-lg" />
+            <button class="btn btn-light mt-3">Search</button>
+          </div>
+
+          <ul class="fa-ul" style="margin-left: 1.65em;">
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">C. las Hermandades, 1, 21750 El
+                Rocío, Huelva</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">clientes@lacampina.es</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">954 844 223</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-md-2 col-lg-4 col-xl-2 mx-auto">
+          <!-- Ayuda -->
+          <h5 class="text-uppercase mb-4 pb-1">
+            Ayuda
+          </h5>
+          <p>
+            <a href="#!" class="text-reset">Preguntas frecuentes</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pagos</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Juego Responsable</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Legal</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Contáctanos</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">Lacampina.es</a>
+    </div>
+
+  </footer>
+
+  </div>
+  <!-- End of .container -->
+  <script src="js/bootstrap.min.js"></script>
+  </div>
+</body>
+
+</html>
