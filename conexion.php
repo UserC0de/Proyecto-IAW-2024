d7546bbd603a92a6a57c4c3c3522cb62e85227a7
Userc0de <userc0demail@gmail.com>
2024-05-02 11:06:13 +0200

    Conexion con la base de datos

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+    // Nueva conexion de la clase mysql
+    // Parámetros: ubicación del servidor, nombre de usuario, contraseña, base de datos
+
+    $mysqli = new mysqli("localhost","root","","casino");
+
+    if ($mysqli -> connect_errno) {
+        echo "Fallo al conectar a MySQL: (", $mysqli->connect_errno ,")" , $mysqli->connect_error;
+    } else {
+        echo "BIEEEEEEEEEEEEEEEEEEEEEEN";
+    }
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  require 'conexion.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
