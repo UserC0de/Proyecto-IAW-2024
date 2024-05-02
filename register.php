35006616eba67398c8102a5510701f355df00208
Userc0de <userc0demail@gmail.com>
2024-05-03 00:06:58 +0200

    Registro

---
+++ register.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="javascript" href="script.js">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="Miestilos.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+Mostrar u ocultar contraseña
+
+<script type="text/javascript">
+    function mostrarPassword() {
+        var cambio = document.getElementById("txtPassword");
+        if (cambio.type == "password") {
+            cambio.type = "text";
+            $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+        } else {
+            cambio.type = "password";
+            $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+        }
+    }
+
+    $(document).ready(function () {
+        //CheckBox mostrar contraseña
+        $('#ShowPassword').click(function () {
+            $('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+        });
+    });
+
+    function mostrarPassword2() {
+        var cambio = document.getElementById("txtPassword2");
+        if (cambio.type == "password") {
+            cambio.type = "text";
+            $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+        } else {
+            cambio.type = "password";
+            $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+        }
+    }
+
+    $(document).ready(function () {
+        //CheckBox mostrar contraseña
+        $('#ShowPassword').click(function () {
+            $('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+        });
+    });
+</script>
+
+
+<body>
+    <section class="vh-100">
+        <div class="container py-5 h-100">
+            <div class="row justify-content-center align-items-center h-100">
+                <div class="col-12 col-lg-9 col-xl-19">
+                    <div
+                        class="card shadow-2-strong card-registration border border-dark rounded-4 bg-dark bg-gradient">
+                        <div class="card-body p-md-5 text-light">
+                            <div class="d-flex justify-content-end">
+                                <a href="index.php" class="btn btn-info text-light">Volver al inicio</a>
+                            </div>
+                            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 d-flex justify-content-center fw-bold">REGISTRO</h3>
+                            <form action="register2.php" method="POST">
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="nombre" class="form-control" required />
+                                            <label class="form-label">Nombre</label>
+                                        </div>
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="apellido" class="form-control" required />
+                                            <label class="form-label">Apellido</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="usuario" class="form-control" required />
+                                            <label class="form-label">Nickname</label>
+                                        </div>
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="dni" class="form-control" pattern="\d{8}[a-zA-Z]" title="El formato del DNI no es válido" required />
+                                            <label class="form-label">DNI</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="direccion" class="form-control" required />
+                                            <label class="form-label">Dirección</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-2 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="pais" class="form-control" required />
+                                            <label class="form-label">Pais</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-2 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="ciudad" class="form-control" required />
+                                            <label class="form-label">Ciudad</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-2 mb-4">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" name="cod_postal" class="form-control" required />
+                                            <label class="form-label">Código Postal</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 d-flex align-items-center">
+                                        <div data-mdb-input-init class="form-outline datepicker w-100">
+                                            <input type="date" name="fecha_nac" class="form-control" required />
+                                            <label class="form-label">Fecha de Nacimiento</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 mb-4">
+                                        <!-- Categoría -->
+                                        <select class="form-control" aria-label="Default select" name="genero" required>
+                                            <option value="Masculino">Hombre</option>
+                                            <option value="Femenino">Mujer</option>
+                                        </select>
+                                        <label class="form-label">Género</label>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 pb-2">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="email" name="email" class="form-control" required />
+                                            <label class="form-label">Email</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="tel" name="telefono" class="form-control" pattern="[0-9]{9}" title="El formato del teléfono no es válido. Debe contener 9 números." required />
+                                            <label class="form-label">Número de teléfono</label>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 mb-4 pb-2">
+                                        <div data-mdb-input-init class="form-outline input-group">
+                                            <input type="password" name="password" id="txtPassword"
+                                                class="form-control" pattern="(?=.*\d)(?=.*[A-Z]).{8,}" title="Mínimo 8 caráteres, 1 mayuscula y 1 número" required />
+                                            <div class="input-group-append">
+                                                <button id="show_password" class="btn btn-primary" type="button"
+                                                    onclick="mostrarPassword()"> <span
+                                                        class="fa fa-eye-slash icon"></span> </button>
+                                            </div>
+                                        </div>
+                                        <label class="form-label">Contraseña</label>
+                                    </div>
+
+                                    <div class="col-md-6 mb-4 pb-2">
+                                        <div data-mdb-input-init class="form-outline input-group">
+                                            <input type="password" name="password2" id="txtPassword2"
+                                                class="form-control" required />
+                                            <div class="input-group-append">
+                                                <button id="show_password" class="btn btn-primary" type="button"
+                                                    onclick="mostrarPassword2()"> <span
+                                                        class="fa fa-eye-slash icon"></span> </button>
+                                            </div>
+                                        </div>
+                                        <label class="form-label">Contraseña</label>
+                                    </div>
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" required>
+                                        <label class="form-check-label" for="terminos_condiciones">
+                                            Acepto los términos y condiciones
+                                        </label>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <input data-mdb-button-init data-mdb-ripple-init
+                                            class="btn btn-success btn-lg bg-gradient" type="submit"
+                                            value="Registrarse" />
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ register2.php
@@ -0,0 +1,169 @@
+<?php
+require 'conexion.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="Miestilos.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body class="container vh-100 d-flex align-items-center justify-content-center">
+    <div class="row bg-dark p-5 rounded-4">
+        <?php
+        require 'conexion.php';
+        $error = false;
+
+        function generarNumeroAleatorio($mysqli, $intentos = 0)
+        {
+            // Si se han intentado 5 veces, imprimir un mensaje y salir
+            if ($intentos >= 5) {
+                echo '<div class="alert alert-danger" role="alert">La base de datos está llena de usuarios.</div>';
+                return false;
+            }
+
+            // Generar un número aleatorio
+            $num_random = rand(0, 999999);
+
+            // Consultar si ya existe un usuario con ese id_usuario
+            $sql = "SELECT COUNT(*) AS count FROM usuarios WHERE id_usuario = '$num_random'";
+            $resultado = $mysqli->query($sql);
+
+            if ($resultado) {
+                $row = $resultado->fetch_assoc();
+                $count = $row['count'];
+                // Si ya existe un usuario con ese id_usuario, generar otro número
+                if ($count > 0) {
+                    return generarNumeroAleatorio($mysqli, $intentos + 1);
+                } else {
+                    return $num_random;
+                }
+            } else {
+                // Manejar el error de la consulta
+                return false;
+            }
+        }
+
+        // Obtener los datos del formulario
+        $nombre = $_POST["nombre"];
+        $usuario = $_POST["usuario"];
+        $apellido = $_POST["apellido"];
+        $dni = $_POST["dni"];
+        $direccion = $_POST["direccion"];
+        $pais = $_POST["pais"];
+        $ciudad = $_POST["ciudad"];
+        $cod_postal = $_POST["cod_postal"];
+        $fecha_nac = $_POST["fecha_nac"];
+        $genero = $_POST["genero"];
+        $email = $_POST["email"];
+        $telefono = $_POST["telefono"];
+        $password = $_POST["password"];
+        $password2 = $_POST["password2"];
+
+        $cumpleanos = new DateTime("$fecha_nac");
+        $hoy = new DateTime();
+        $edad = $hoy->diff($cumpleanos);
+        $edad_str = $edad->format('%y');
+
+        // Verificar si el usuario tiene al menos 18 años
+        if ($edad_str < 18) {
+            echo '<div class="alert alert-danger" role="alert">Debes tener al menos 18 años para registrarte.</div>';
+            $error = true;
+        }
+        // Ejemplo de uso:
+        $id_usuario = generarNumeroAleatorio($mysqli);
+
+        if (!$id_usuario) {
+            echo "<div class='alert alert-danger' role='alert'>No se pudo generar un ID de usuario único.</div>";
+            echo "<div class='d-flex justify-content-center'>";
+            echo "<p><a href='register.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+            echo "</div>";
+            exit();
+        }
+
+        // Verificar si las contraseñas coinciden
+        if ($password === $password2) {
+            // Verificar si ya existe un usuario con el mismo nickname
+            $sql = "SELECT * FROM usuarios WHERE nickname = '$usuario'";
+            $resultado = $mysqli->query($sql);
+            if ($resultado->num_rows > 0) {
+                echo '<div class="alert alert-danger" role="alert">Ya existe un usuario con el mismo nickname.</div>';
+                $error = true;
+            }
+
+            // Verificar si ya existe un usuario con el mismo correo electrónico
+            $sql = "SELECT * FROM usuarios WHERE correo = '$email'";
+            $resultado = $mysqli->query($sql);
+            if ($resultado->num_rows > 0) {
+                echo '<div class="alert alert-danger" role="alert">Ya existe un usuario con el mismo correo electrónico.</div>';
+                $error = true;
+            }
+
+            // Verificar si ya existe un usuario con el mismo DNI
+            $sql = "SELECT * FROM usuarios WHERE dni = '$dni'";
+            $resultado = $mysqli->query($sql);
+            if ($resultado->num_rows > 0) {
+                echo '<div class="alert alert-danger" role="alert">Ya existe un usuario con el mismo DNI.</div>';
+                $error = true;
+            }
+
+            // Verificar si ya existe un usuario con el mismo número de teléfono
+            $sql = "SELECT * FROM usuarios WHERE telefono = '$telefono'";
+            $resultado = $mysqli->query($sql);
+            if ($resultado->num_rows > 0) {
+                echo '<div class="alert alert-danger" role="alert">Ya existe un usuario con el mismo número de teléfono.</div>';
+                $error = true;
+            }
+
+            // Si no se encontraron errores, proceder con el insert
+            if (!$error) {
+                $sql = "INSERT INTO usuarios (id_usuario, nombre, apellido, nickname, dni, direccion, pais, ciudad, cod_postal, edad, genero, correo, telefono, saldo, estado, rol_usuario) 
+                        VALUES ('$id_usuario', '$nombre', '$apellido', '$usuario', '$dni', '$direccion', '$pais', '$ciudad', '$cod_postal', '$edad_str', '$genero', '$email', '$telefono', '100', 'A', 'usuario')";
+                $resultado = $mysqli->query($sql);
+
+                if ($resultado) {
+                    // Hash de la contraseña
+                    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+                    // Insertar la contraseña en la tabla passwords
+                    $sql = "INSERT INTO passwords (id_usuario, contrasena) VALUES ('$id_usuario', '$hashed_password')";
+                    $resultado = $mysqli->query($sql);
+
+                    if ($resultado) {
+                        echo '<div class="alert alert-success" role="alert">Se ha registrado con éxito.</div>';
+                        echo "<div class='d-flex justify-content-center'>";
+                        echo "<p><a href='index.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+                        echo "</div>";
+                    } else {
+                        echo '<div class="alert alert-danger" role="alert">Ha habido un error al insertar la contraseña en la tabla passwords.</div>';
+                        echo "<div class='d-flex justify-content-center'>";
+                        echo "<p><a href='register.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo '<div class="alert alert-danger" role="alert">Ha habido un error al registrar el usuario.</div>';
+                    echo "<div class='d-flex justify-content-center'>";
+                    echo "<p><a href='register.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+                    echo "</div>";
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Las contraseñas no coinciden.</div>';
+                echo "<div class='d-flex justify-content-center'>";
+                echo "<p><a href='register.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+                echo "</div>";
+            }
+        } else {
+            echo "<div class='d-flex justify-content-center'>";
+            echo "<p><a href='register.php'><button type='button' class='btn btn-primary'>Volver</button></a></p>";
+            echo "</div>";
+        }
+        ?>
+
+</body>
+</div>
+
+</html>
